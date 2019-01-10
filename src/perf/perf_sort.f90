4b7d74369985ce32c83e14d3af8a64c8d8f9a2f6
Andrey Prokopenko <prokopenkoav@ornl.gov>
2019-01-10 09:00:20 -0500

    perf_sort: added reading parameters from command line

--- src/perf/perf_sort.f90
+++ src/perf/perf_sort.f90
@@ -15,41 +15,66 @@ program main
 
     implicit none
     integer :: i, k, n
-    integer, parameter :: num_loops = 40
-    integer, parameter :: n0 = 10000
-    integer, parameter :: steps = 4
+    integer :: num_loops = 40
+    integer :: n0 = 10000
+    integer :: num_steps = 1
     real(c_double), dimension(:), allocatable :: x
+    character(len=256) :: arg
     type(Timer) :: t
+
+    ! Read in command line arguments
+    k = 1
+    do while (k <= command_argument_count())
+        call get_command_argument(k, arg)
+        k = k+1
+
+        select case (arg)
+            case ('-l', '--loop')
+                call get_command_argument(k, arg)
+                k = k+1
+                read(arg,*) num_loops
+            case ('-n')
+                call get_command_argument(k, arg)
+                k = k+1
+                read(arg,*) n0
+            case ('-s', '--steps')
+                call get_command_argument(k, arg)
+                k = k+1
+                read(arg,*) num_steps
+        end select
+    end do
+    write(0,*) "num_loops: ", num_loops
+    write(0,*) "n0       : ", n0
+    write(0,*) "num_steps: ", num_steps
 
     call t%create()
 
-
     n = n0
-    do k = 1, steps
+    do k = 1, num_steps
         ! Allocate array
         allocate(x(n))
 
         ! Fortran version of quick sort
         do i = 1, num_loops
-            call fill(x)                ! reset array
+            call fill(x)    ! reset array
 
             call t%start()
             call quick_sort(x, n)
             call t%stop()
         enddo
-        write(0, '(A, I8, F10.5)') "Fortran :", n, t%walltime() / num_loops
+        write(0,'(A,I8,A,F10.5,A,F10.5,A)') "Fortran : n = ", n, ", avg = ", t%walltime() / num_loops, ", total = ", t%walltime()
 
         call t%reset()
 
         ! Wrapped C++ version of quick sort
         do i = 1, num_loops
-            call fill(x)                ! reset array
+            call fill(x)    ! reset array
 
             call t%start()
             call sort(x)
             call t%stop()
         enddo
-        write(0, '(A, I8, F10.5)') "C++     :", n, t%walltime() / num_loops
+        write(0,'(A,I8,A,F10.5,A,F10.5,A)') "C++     : n = ", n, ", avg = ", t%walltime() / num_loops, ", total = ", t%walltime()
 
         deallocate(x)
 
